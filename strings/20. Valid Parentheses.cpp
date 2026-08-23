875ad6ab80e03500dabcd12d31acca8eb1d796aa
Adarsha231 <adarshanaik204@gmail.com>
2026-08-23 16:58:30 +0530

    Add solution for valid parentheses problem
    Implement a function to check if parentheses are valid.

---
+++ strings/20. Valid Parentheses.cpp
@@ -0,0 +1,81 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+Example 5:
+
+Input: s = "([)]"
+
+Output: false
+
+ 
+*/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n=s.length();
+        if(n==1)
+        {
+          return false;
+        }
+        for(char ch:s)
+        {
+            if( ch=='('||ch=='['||ch=='{')
+            {
+                st.push(ch);
+
+            }
+            else
+            {   
+                if(st.empty())
+                {
+                  return false;
+                }
+
+                if(ch==')'&&st.top()=='('||ch==']'&&st.top()=='['||ch=='}'&&st.top()=='{')
+                {
+                    st.pop();
+                }  
+                else
+                {
+                    return false;
+                } 
+                
+            }
+
+        }
+        return st.empty();
+    }
+};
